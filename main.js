169622ec726b7c6a16cf3e87b8cb19b48c086cd8
bmbmbmbmbmbmbmbmbmb <116736949+bmbmbmbmbmbmbmbmbmb@users.noreply.github.com>
2023-04-18 11:05:04 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const togglebtn = document.getElementById('toggle-mode');
-const body = document.body;
-
-
-togglebtn.addEventListener('click', function () {
-    body.classList.toggle('dark-mode')
-    heder.classList.toggle('dark-mode-header')
-})
